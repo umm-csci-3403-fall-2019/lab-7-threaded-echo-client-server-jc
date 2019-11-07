eb49d3208f1ef851c607e825cd721374478179f4
JoshuaHamann <haman127@morris.umn.edu>
2019-11-06 21:01:20 -0600

    we added a second threader to the client and changed the thread in and out accordingly

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,34 +1,32 @@
 package echoserver;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.lang.Thread;
 import java.net.Socket;
 
-public class EchoClient implements Runnable{
+public class EchoClient {
 	public static final int PORT_NUMBER = 6013;
 	public static String server;
 
 	public static void main(String[] args) throws IOException {
-		if (args.length > 0) {
-			server = args[0];
-		} else {
-			server = "127.0.0.1";
-		}
+//		if (args.length > 0) {
+//			server = args[0];
+//		} else {
+//			server = "127.0.0.1";
+//		}
 		EchoClient client = new EchoClient();
 		client.start();
 	}
 
-	public void run(){}
-
 	private void start() throws IOException {
 
 		Socket socket = new Socket(server, PORT_NUMBER);
-		OutputStream writer = socket.getOutputStream();
-		InputStream input = socket.getInputStream();
-		Thread threader = new Thread(new EchoClientThreadIn(System.in, writer, socket));
+		EchoClientThreadIn input = new EchoClientThreadIn(socket);
+		EchoClientThreadOut output = new EchoClientThreadOut(socket);
+		Thread threader = new Thread(input);
+		Thread threader2 = new Thread(output);
 		threader.start();
-
+		threader2.start();
 
 	}
 
