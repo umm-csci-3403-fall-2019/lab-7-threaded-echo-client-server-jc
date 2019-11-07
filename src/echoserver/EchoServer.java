49c558a3454be04d9376d7988ebb371e7de676b2
JoshuaHamann <haman127@morris.umn.edu>
2019-11-06 21:05:19 -0600

    simplified to constantly be looking for a client thread

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,42 +1,32 @@
 package echoserver;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.lang.Thread;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class EchoServer implements Runnable{
+public class EchoServer {
+
 	
 	// REPLACE WITH PORT PROVIDED BY THE INSTRUCTOR
 	public static final int PORT_NUMBER = 6013;
-
 	public static void main(String[] args) throws IOException, InterruptedException {
 		EchoServer server = new EchoServer();
 		server.start();
 	}
 
-	public void run() {}
-
 	private void start() throws IOException, InterruptedException {
 		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
-		final ExecutorService pool;
-		pool = Executors.newFixedThreadPool(1);
+		//final ExecutorService pool;
+		//pool = Executors.newFixedThreadPool(20);
 		while (true) {
 			Socket socket = serverSocket.accept();
-			OutputStream writer = socket.getOutputStream();
-			InputStream input = socket.getInputStream();
-			Thread threader = new Thread(new EchoClientThreadOut(input, writer, socket));
+			EchoServerThreader server = new EchoServerThreader(socket);
+			Thread threader = new Thread(server);
 			threader.start();
-
-			// Put your code here.
-			// This should do very little, essentially:
-			//   * Construct an instance of your runnable class
-			//   * Construct a Thread with your runnable
-			//      * Or use a thread pool
-			//   * Start that thread
 		}
 	}
+
 }
